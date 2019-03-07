771c535b92986e237aad928fa867dd31a868219c
jokopriyono <joe.mandrake007@gmail.com>
2019-03-08 00:37:21 +0700

    Mengubah tampilan awal ke Menu

--- app/src/main/java/com/jokopriyono/eudeka/mvvm/view/menu/MenuActivity.kt
+++ app/src/main/java/com/jokopriyono/eudeka/mvvm/view/menu/MenuActivity.kt
@@ -3,11 +3,20 @@ package com.jokopriyono.eudeka.mvvm.view.menu
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.jokopriyono.eudeka.mvvm.R
+import com.jokopriyono.eudeka.mvvm.view.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_menu.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.startActivity
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        card_football_team.onClick {
+            startActivity<LoginActivity>()
+            finish()
+        }
     }
 }
